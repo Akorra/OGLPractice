555179cab673ebdb9cacc4d1fa38f73939fe1295
Phil Marquez <neverfearphilishere@gmail.com>
2026-05-15 21:55:59 +0100

    texture stuff

--- include/mesh.hpp
+++ include/mesh.hpp
@@ -137,3 +137,63 @@ Mesh generateRectangle(bool colors=true, bool normals=true, bool st=true)
 
     return rectangle;
 }
+
+/**
+ * Texture Wrapping
+ * 
+ * glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+ * glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+ * 
+ * args: 
+ *  - texture target (image is 2D)
+ *  - target axis
+ *  - texture wrapping mode
+ * 
+ * texture coordinates range (0,0) to (1,1), but may go outside range, so we introduce texture wrapping:
+ *  - GL_REPEAT: The default behavior for textures. Repeats the texture image.
+ *  - GL_MIRRORED_REPEAT: Same as GL_REPEAT but mirrors the image with each repeat.
+ *  - GL_CLAMP_TO_EDGE: Clamps the coordinates between 0 and 1. The result is that higher coordinates become clamped to the edge, resulting in a stretched edge pattern.
+ *  - GL_CLAMP_TO_BORDER: Coordinates outside the range are now given a user-specified border color.
+ *      - this option also requires color: glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, {r,g,b,a})
+ */
+
+ /**
+  * Texture Filtering
+  * 
+  * determine wich texture pixel (texel) a float texture coordinate belongs to.
+  * 
+  * GL_NEAREST (nearest neightbour or point filtering) select texel with center closest to coordinate (default) - blocky
+  * GL_LINEAR  ((bi)linear filtering) interpolated value from coordinates neightboring texels. - smoother
+  * 
+  * They can be set for scaling up or down independently:
+  * glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  * glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  */
+
+  /**
+   * Mipmaps
+   * 
+   * textured objects small/far-away, with few fragments, produce artifacts due to ogl difficulty retrieving color.
+   * waste of memory bandwidth
+   * 
+   * Mipmaps introduce continuously size-halved versions of the image, allowing to pick the best suited one for an object 
+   * depending ond size/amount of fragments produced.
+   * 
+   * glGenerateMipmap - create mipmaps
+   * options:
+   *  - GL_NEAREST_MIPMAP_NEAREST: takes the nearest mipmap to match the pixel size and uses nearest neighbor interpolation for texture sampling.
+   *  - GL_LINEAR_MIPMAP_NEAREST: takes the nearest mipmap level and samples that level using linear interpolation.
+   *  - GL_NEAREST_MIPMAP_LINEAR: linearly interpolates between the two mipmaps that most closely match the size of a pixel and samples the interpolated level via nearest neighbor interpolation.
+   *  - GL_LINEAR_MIPMAP_LINEAR: linearly interpolates between the two closest mipmaps and samples the interpolated level via linear interpolation.
+   * 
+   * glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+   * glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); // mipmap has no effect on magnification
+   */
+
+   /**
+    * loading and creating textures:
+    * 
+    * stb_image.h - single header image loading library by Sean Barrett
+    *   #define STB_IMAGE_IMPLEMENTATION
+    *   #include "stb_image.h"
+    */
