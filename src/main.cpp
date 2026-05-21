6beec18926d8a1f38932e04f159fb6a695e04880
Phil Marquez <neverfearphilishere@gmail.com>
2026-05-21 23:30:42 +0100

    stencil testing - outline

--- src/main.cpp
+++ src/main.cpp
@@ -43,6 +43,18 @@ bool firstMouse = true;
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
+/**
+ * Outline via stencil and depth testing:
+ * 1. Enable stencil writing.
+ * 2. Set the stencil op to GL_ALWAYS before drawing the (to be outlined) objects, updating the stencil buffer with 1s wherever the objects' fragments are rendered.
+ * 3. Render the objects.
+ * 4. Disable stencil writing and depth testing.
+ * 5. Scale each of the objects by a small amount.
+ * 6. Use a different fragment shader that outputs a single (border) color.
+ * 7. Draw the objects again, but only if their fragments' stencil values are not equal to 1.
+ * 8. Enable depth testing again and restore stencil func to GL_KEEP.
+ */
+
 int main()
 {
     // glfw: initialize and configure
@@ -81,9 +93,13 @@ int main()
     // configure global opengl state
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS);
+    glEnable(GL_STENCIL_TEST);
+    glStencilFunc(GL_NOTEQUAL, 1, 0xFF);
+    glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
 
     // build and compile shaders
-    Shader shader("./shaders/depthTest.vs.glsl", "./shaders/depthTest.fs.glsl");
+    Shader shader("./shaders/stencilTesting.vs.glsl", "./shaders/stencilTesting.fs.glsl");
+    Shader shaderOutline("./shaders/stencilTesting.vs.glsl", "./shaders/stencilTestingColor.fs.glsl");
     
     // cube vao
     uint32_t cubeVAO, cubeVBO;
@@ -138,14 +154,33 @@ int main()
 
         // render
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
-        shader.use();
+        //set uniforms
         view = camera.GetViewMatrix();
         projection = glm::perspective(glm::radians(camera.zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, NEAR_PLANE, FAR_PLANE);
+        
+        shaderOutline.use();
+        shaderOutline.setMat4("projection", projection);
+        shaderOutline.setMat4("view",       view);
+
+        shader.use();
         shader.setMat4("projection", projection);
         shader.setMat4("view",       view);
-        
+
+        // draw floor as normal, but don't write the floor to the stencil buffer, we only care about the containers. We set its mask to 0x00 to not write to the stencil buffer.
+        glStencilMask(0x00);
+        // floor
+        glBindVertexArray(planeVAO);
+        glBindTexture(GL_TEXTURE_2D, floorTexture);
+        shader.setMat4("model", glm::mat4(1.0f));
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glBindVertexArray(0);
+
+        // 1st. render pass, draw objects as normal, writing to the stencil buffer
+        glStencilFunc(GL_ALWAYS, 1, 0xFF);
+        glStencilMask(0xFF);
+
         // cubes
         glBindVertexArray(cubeVAO);
         glActiveTexture(GL_TEXTURE0);
@@ -159,12 +194,34 @@ int main()
         shader.setMat4("model", model);
         glDrawArrays(GL_TRIANGLES, 0, cubeVertCount);
 
-        // floor
-        glBindVertexArray(planeVAO);
-        glBindTexture(GL_TEXTURE_2D, floorTexture);
-        shader.setMat4("model", glm::mat4(1.0f));
-        glDrawArrays(GL_TRIANGLES, 0, planeVertCount);
+        // 2nd. render pass: now draw slightly scaled versions of the objects, this time disabling stencil writing.
+        // Because the stencil buffer is now filled with several 1s. The parts of the buffer that are 1 are not drawn, thus only drawing 
+        // the objects' size differences, making it look like borders.
+        glStencilFunc(GL_NOTEQUAL, 1, 0xFF);
+        glStencilMask(0x00);
+        glDisable(GL_DEPTH_TEST);
+
+        shaderOutline.use();
+
+        float scale = 1.1f;
+
+        // cubes
+        glBindVertexArray(cubeVAO);
+        glBindTexture(GL_TEXTURE_2D, cubeTexture);
+        // cube 1
+        model = glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(-1.0f, 0.0f, -1.0f)), glm::vec3(scale));
+        shaderOutline.setMat4("model", model);
+        glDrawArrays(GL_TRIANGLES, 0, cubeVertCount);
+        //cube 2
+        model = glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(2.0f, 0.0f, 0.0f)), glm::vec3(scale));
+        shaderOutline.setMat4("model", model);
+        glDrawArrays(GL_TRIANGLES, 0, cubeVertCount);
+
         glBindVertexArray(0);
+
+        glStencilMask(0xFF);
+        glStencilFunc(GL_ALWAYS, 0, 0xFF);
+        glEnable(GL_DEPTH_TEST);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         glfwSwapBuffers(window);
