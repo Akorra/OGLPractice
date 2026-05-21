5c37943a487fd30604ba88f27691892ddfe28a4b
Phil Marquez <neverfearphilishere@gmail.com>
2026-05-21 10:56:36 +0100

    moved unused files

---
+++ archive/simple.fs.glsl
@@ -0,0 +1,14 @@
+#version 330 core
+out vec4 FragColor;
+  
+in vec3 vertexColor; // the input variable from the vertex shader (same name and same type)  
+in vec2 TexCoord;
+
+uniform float variance; // we set this variable in the OpenGL code.
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
+void main()
+{
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, vec2(TexCoord.x, TexCoord.y)), variance);
+}
---
+++ archive/simple.vs.glsl
@@ -0,0 +1,18 @@
+#version 330 core
+layout (location = 0) in vec3 aPos;   // the position variable has attribute position 0
+layout (location = 1) in vec4 aColor; // the color variable has attribute position 1
+layout (location = 2) in vec2 aTexCoord;
+
+out vec3 vertexColor; // specify a color output to the fragment shader
+out vec2 TexCoord;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+void main()
+{
+    gl_Position = projection*view*model*vec4(aPos, 1.0); // local to clip
+    vertexColor = aColor.rgb; // set the output variable to a dark-red color
+    TexCoord    = aTexCoord;
+}
