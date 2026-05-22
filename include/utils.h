372929b724dd7ed78993fa5652a4f29d12ba0aad
Phil Marquez <neverfearphilishere@gmail.com>
2026-05-22 14:40:50 +0100

    new project structure

--- include/utils.h
+++ include/utils.h
@@ -1,49 +1,55 @@
 #pragma once
 
+#include <glm/glm.hpp>
+
+#include <glad/gl.h>
+
+#include "shader.hpp"
+
 inline constexpr unsigned int cubeVertCount = 36;
 inline constexpr float cube[] = {
-    // positions            // texture coords
-    -0.5f, -0.5f, -0.5f,    0.0f, 0.0f,
-     0.5f, -0.5f, -0.5f,    1.0f, 0.0f,
-     0.5f,  0.5f, -0.5f,    1.0f, 1.0f,
-     0.5f,  0.5f, -0.5f,    1.0f, 1.0f,
-    -0.5f,  0.5f, -0.5f,    0.0f, 1.0f,
-    -0.5f, -0.5f, -0.5f,    0.0f, 0.0f,
-
-    -0.5f, -0.5f,  0.5f,    0.0f, 0.0f,
-     0.5f, -0.5f,  0.5f,    1.0f, 0.0f,
-     0.5f,  0.5f,  0.5f,    1.0f, 1.0f,
-     0.5f,  0.5f,  0.5f,    1.0f, 1.0f,
-    -0.5f,  0.5f,  0.5f,    0.0f, 1.0f,
-    -0.5f, -0.5f,  0.5f,    0.0f, 0.0f,
-
-    -0.5f,  0.5f,  0.5f,    1.0f, 0.0f,
-    -0.5f,  0.5f, -0.5f,    1.0f, 1.0f,
-    -0.5f, -0.5f, -0.5f,    0.0f, 1.0f,
-    -0.5f, -0.5f, -0.5f,    0.0f, 1.0f,
-    -0.5f, -0.5f,  0.5f,    0.0f, 0.0f,
-    -0.5f,  0.5f,  0.5f,    1.0f, 0.0f,
-
-     0.5f,  0.5f,  0.5f,    1.0f, 0.0f,
-     0.5f,  0.5f, -0.5f,    1.0f, 1.0f,
-     0.5f, -0.5f, -0.5f,    0.0f, 1.0f,
-     0.5f, -0.5f, -0.5f,    0.0f, 1.0f,
-     0.5f, -0.5f,  0.5f,    0.0f, 0.0f,
-     0.5f,  0.5f,  0.5f,    1.0f, 0.0f,
-
-    -0.5f, -0.5f, -0.5f,    0.0f, 1.0f,
-     0.5f, -0.5f, -0.5f,    1.0f, 1.0f,
-     0.5f, -0.5f,  0.5f,    1.0f, 0.0f,
-     0.5f, -0.5f,  0.5f,    1.0f, 0.0f,
-    -0.5f, -0.5f,  0.5f,    0.0f, 0.0f,
-    -0.5f, -0.5f, -0.5f,    0.0f, 1.0f,
-
-    -0.5f,  0.5f, -0.5f,    0.0f, 1.0f,
-     0.5f,  0.5f, -0.5f,    1.0f, 1.0f,
-     0.5f,  0.5f,  0.5f,    1.0f, 0.0f,
-     0.5f,  0.5f,  0.5f,    1.0f, 0.0f,
-    -0.5f,  0.5f,  0.5f,    0.0f, 0.0f,
-    -0.5f,  0.5f, -0.5f,    0.0f, 1.0f
+    // Back face
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, // Bottom-left
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-right
+     0.5f, -0.5f, -0.5f,  1.0f, 0.0f, // bottom-right         
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-right
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, // bottom-left
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, // top-left
+    // Front face
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-left
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // bottom-right
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f, // top-right
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f, // top-right
+    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f, // top-left
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-left
+    // Left face
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // top-right
+    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-left
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // bottom-left
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // bottom-left
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-right
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // top-right
+    // Right face
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // top-left
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // bottom-right
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-right         
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // bottom-right
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // top-left
+     0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-left     
+    // Bottom face
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // top-right
+     0.5f, -0.5f, -0.5f,  1.0f, 1.0f, // top-left
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // bottom-left
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // bottom-left
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-right
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // top-right
+    // Top face
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, // top-left
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // bottom-right
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-right     
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // bottom-right
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, // top-left
+    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f  // bottom-left
 };
 
 // we set these higher than 1 (together with GL_REPEAT as texture wrapping mode). this will cause the floor texture to repeat
